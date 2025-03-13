a5902dd164cd67d414622290394e5c2ef8b35067
okechukwu david <david@koonage.com>
2025-03-13 07:55:11 +0100

    added controlled input component to login paged

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React from "react";
-import CustomInput from "../../../../components/CustomInput";
+
 // import { useForm, SubmitHandler } from "react-hook-form";
 import { SubmitHandler, useForm } from "react-hook-form";
+import ControlledInput from "../../../../components/ControlledInput";
 
 interface IFormInputs {
   email: string;
@@ -10,47 +11,28 @@ interface IFormInputs {
 }
 
 const Login = () => {
-  const {
-    register,
-    clearErrors,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<IFormInputs>();
+  const { handleSubmit, control } = useForm<IFormInputs>();
   const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data);
   return (
     <div className="space-y-3">
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 mt-16 ">
-        <CustomInput
+        <ControlledInput
+          placeholder="Enter Email"
           label="Email"
-          placeholder="Enter your Email"
-          {...register("email", { required: "Email Address is required" })}
-          aria-invalid={errors.email ? "true" : "false"}
-          onChange={() => clearErrors("email")}
+          control={control}
+          type="email"
+          name="email"
+          rules={{ required: "email is required" }}
         />
-        {errors.email && (
-          <p
-            className="text-red-950 playfair text-[15px] font-medium -my-2"
-            role="alert"
-          >
-            {errors.email?.message}
-          </p>
-        )}
-        <CustomInput
+
+        <ControlledInput
+          placeholder="Enter password"
+          label="password"
+          control={control}
           type="password"
-          label="Password"
-          placeholder="Enter Your Password"
-          {...register("password", { required: "Password is required" })}
-          aria-invalid={errors.password ? "true" : "false"}
-          onChange={() => clearErrors("password")}
+          name="password"
+          rules={{ required: "password is required" }}
         />
-        {errors.password && (
-          <p
-            className="text-red-950 playfair text-[15px] font-medium -my-2"
-            role="alert"
-          >
-            {errors.password?.message}
-          </p>
-        )}
         <div className="pt-5">
           <button className="bg-gws w-[12.5rem] rounded-[0.625rem] py-3 text-white playfair">
             Submit
